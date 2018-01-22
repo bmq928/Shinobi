d9af1e61556c535d02bbec5c297db2a26c80af66
Bui Minh Quang <bmq928@gmail.com>
2018-01-22 14:10:25 +0700

    replace map by search and logs

--- app/common/directives/frames/frames.controller.js
+++ app/common/directives/frames/frames.controller.js
@@ -3,13 +3,18 @@
         .module('app')
         .controller('framesCtrl', framesCtrl)
 
-    framesCtrl.$inject = ['$rootScope']
-    function framesCtrl($rootScope){
+    framesCtrl.$inject = ['$rootScope', '$uibModal', 'mapSrc']
+    function framesCtrl($rootScope, $uibModal, mapSrc){
         var vm = this;
 
         vm.changeSelectedCam = function(cam){
             $rootScope.$emit($rootScope.events.changeSelectedCam, cam);
         }
 
+        vm.showMapAllCam = function(){
+            $uibModal.open({
+                template: '<img src="' + mapSrc.getAll() +  '" alt="Map">'
+            })
+        }
     }
 })()
