c50d09b1969aa02e0bec3fce88bd859a987d23fc
Bui Minh Quang <bmq928@gmail.com>
2018-02-08 17:41:01 +0700

    fix server.js to use shinobi.api

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('path');
+const apiRouter = require('./shinobi-api/src/routers/api');
 
 let app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'client_app')));
 
+app.use('/api', apiRouter);
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 })
