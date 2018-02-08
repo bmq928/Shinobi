c118f6c41208a699bb0aea61b63b9f1a464ac37c
Bui Minh Quang <bmq928@gmail.com>
2018-02-08 17:26:02 +0700

    add temporary api

---
+++ shinobi-api/src/routers/api.js
@@ -0,0 +1,49 @@
+var express = require('express');
+var router = express.Router();
+var ctrlImage = require('../controllers/video');
+
+router.get('/', function(req, res) {
+	res.send("This is api router for Shinobi");
+	res.end();
+});
+
+router.get('/image', function(req, res) {
+	let requestStreamFromShinobi = ctrlImage.image();
+	requestStreamFromShinobi.pipe(res);
+});
+
+router.get('/video/list/:groupKey', function(req, res) {
+	ctrlImage.videoOfGroupKey(function(listVideos) {
+		console.log(listVideos);
+		res.send(listVideos);
+		res.end();
+	});
+	
+	// ctrlImage.videoOfGroupKey(function(href) {
+	// 	console.log(href);
+	// 	res.send(href);
+	// 	res.end();
+	// });
+})
+
+router.get('/video/list/:monitorId', function(req, res) {
+	ctrlImage.videoOfGroupKey(function(listVideos) {
+		console.log(listVideos);
+		res.send(listVideos);
+		res.end();
+	});
+	
+})
+
+router.get('/video/:fileName', function(req, res) {
+	let href = '/FcuQHjPyqXOQtVSDXuFH5kFzeSEydE/videos/uet/p404/2018-01-19T14-00-07.webm';
+	let video = ctrlImage.getVideo(href);
+	video.pipe(res);
+})
+
+router.delete('/video/:fileName/delete', function(req, res) {
+	let href = '/FcuQHjPyqXOQtVSDXuFH5kFzeSEydE/videos/uet/p404/2018-01-19T14-00-07.webm';
+	let video = ctrlImage.deleteVideo(href);
+	video.pipe(res);
+})
+module.exports = router;
