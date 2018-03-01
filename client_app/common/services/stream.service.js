aec941984c8d346e9245865f7272f0ea65762da0
Bui Minh Quang <bmq928@gmail.com>
2018-03-01 17:44:31 +0700

    add stream

--- client_app/common/services/stream.service.js
+++ client_app/common/services/stream.service.js
@@ -1,10 +1,27 @@
-(function(){
+(function () {
     angular
         .module('app')
         .factory('stream', stream)
 
     stream.$inject = ['$resource']
-    function stream($resource){
+    function stream($resource) {
+        var rootUrl = 'http://10.0.9.252:252'
+        var apiKey = 'FcuQHjPyqXOQtVSDXuFH5kFzeSEydE';
+        
+        
+        return {
+            byMonitorId: function (mid, groupKey) {
+                mid = mid || 'p404'
+                groupKey = groupKey || 'uet'
+                // return rootUrl + '/' + apiKey +  '/embed/' + groupKey + '/' + mid + '/full';
+                return rootUrl + '/dd96563a40ace53657f75b44197a0649/mjpeg/uet/p404/full'
 
+                // var url = rootUrl + '/' + apiKey +  '/embed/' + groupKey + '/' + mid + '/jquery|fullscreen';
+                // var promise = $resource(url).get().$promise;
+                // console.log(promise);
+
+                // return promise;
+            }
+        }
     }
 })()
--- client_app/live/live.controller.js
+++ client_app/live/live.controller.js
@@ -1,9 +1,25 @@
-(function(){
+(function () {
     angular
         .module('app')
         .controller('liveCtrl', liveCtrl)
 
-    function liveCtrl(){
+    //$sce to inject url to iframe tag
+    liveCtrl.$inject = ['stream', '$sce']
+    function liveCtrl(stream, $sce) {
+        var vm = this;
+        vm.links = []
+        vm.isLoading = isLoading()
 
+        // vm.links.push(stream.byMonitorId())
+        // console.log(vm.links)
+
+        //just for demo
+        vm.link = $sce.trustAsResourceUrl(stream.byMonitorId())
+
+
+
+        function isLoading() {
+            return vm.links.length === 0;
+        }
     }
 })()
