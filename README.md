5155de4ab1e4c1997be1895ff2488dae72024f2d
Bui Minh Quang <bmq928@gmail.com>
2018-03-06 11:24:18 +0700

    add add default value in config view

--- README.md
+++ README.md
@@ -3,5 +3,5 @@ use $rootScope to store the state of app
 component only emit the event to $rootScope and get value
 only appCtrl set data of $rootScope
 
-app.config : store state of $rootScope in run function
+app.config : store state of $rootScope in run function and share constant in app
 app.controller: get the event and handle the change of $rootScope
--- client_app/app.config.js
+++ client_app/app.config.js
@@ -59,27 +59,33 @@
                             {
                                 name: "Mode",
                                 description: "This is the primary task of the monitor.",
-                                selectOption: []
+                                selectOption: ['record', 'disable', 'watch-only', 'idle'],
+                                curVal: 'record'
                             }, {
                                 name: "Monitor ID",
                                 description: "This is a non-changeable identifier for the monitor. You can duplicate a monitor by double clicking the Monitor ID and changing it.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: 'p404'
                             }, {
                                 name: "Name",
                                 description: "This is the human-readable display name for the monitor.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: 'p404'
                             }, {
                                 name: "Retry Connection",
                                 description: "The number of times to retry for network connection between the server and camera before setting the monitor to Disabled. No decimals. Set to 0 to retry forever.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: ''
                             }, {
                                 name: "Notes",
                                 description: "Comments you want to leave for this camera.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: ''
                             }, {
                                 name: "Storage Location",
                                 description: "",
-                                selectOption: ['option1', 'option2', 'option3']
+                                selectOption: ['main', 'second'],
+                                curVal: 'main'
                             }
                         ]
                     }, {
@@ -89,27 +95,99 @@
                             {
                                 name: "Input Type",
                                 description: "The method that will used to consume the video stream.",
-                                selectOption: ['option1', 'option2', 'option3']
+                                selectOption: ['jpeg', 'mjpeg', 'h.264/h.265/h.265+', 'hls(.m3u8)', 'mpeg-4(.mp4, .ts)', 'shinobi streamer', 'dashcam(streamer v2)', 'local'],
+                                curVal: 'h.264/h.265/h.265+'
                             }, {
-                                name: "Monitor ID ",
+                                name: "Connection Type ",
+                                description: "The protocol that will used to consume the video stream.",
+                                selectOption: ['http', 'https', 'rtsp', 'udp'],
+                                curVal: 'rtsp'
+                            }, {
+                                name: "RTSP Transport",
+                                description: "The transport protocol your camera will use. TCP is usually the best choice.",
+                                selectOption: ['auto', 'tcp', 'udp', 'http'],
+                                curVal: 'tcp'
+                            }, {
+                                name: "Username",
+                                description: "The user login for your camera.",
+                                selectOption: [],
+                                curVal: 'admin'
+                            }, {
+                                name: "Password",
+                                description: "The password for your camera.",
+                                selectOption: [],
+                                curVal: '123456'
+                            }, {
+                                name: "Host",
+                                description: "Connection address",
+                                selectOption: [],
+                                curVal: '10.0.9.253'
+                            }, {
+                                name: "Port",
+                                description: "Port number that your camera is streaming out on.",
+                                selectOption: [],
+                                curVal: '10.0.9.253'
+                            }, {
+                                name: "Force Port",
+                                description: "Using the default web port can allow automatic switch to other ports for streams like RTSP.",
+                                selectOption: [true, false],
+                                curVal: false
+                            }, {
+                                name: "Path",
+                                description: "The path to your camera",
+                                selectOption: [],
+                                curVal: '/Streaming/Channels/101'
+                            }, {
+                                name: "Analyzation Duration",
+                                description: "Specify how many microseconds are analyzed to probe the input. Set to 100000 if you are using RTSP and having stream issues.",
+                                selectOption: [],
+                                curVal: 100000
+                            }, {
+                                name: "Probe Size",
+                                description: "Specify how big to make the analyzation probe for the input. Set to 100000 if you are using RTSP and having stream issues",
+                                selectOption: [],
+                                curVal: ''
+                            }, {
+                                name: "Accelerator",
+                                description: "",
+                                selectOption: [true, false],
+                                curVal: ''
+                            }
+                        ]
+                    }, {
+                        title: "Stream",
+                        description: "This section will designate the primary stream out method and it's settings. This stream will be displayed in the dashboard. If you choose to use HLS, JPEG, or MJPEG then you can consume the stream through other programs.",
+                        options: [
+                            {
+                                name: "Stream Type",
+                                description: "The method that will used to consume the video stream.",
+                                selectOption: ['record', 'disable', 'watch-only', 'idle'],
+                                curVal: 'record'
+                            }, {
+                                name: "Monitor ID",
                                 description: "This is a non-changeable identifier for the monitor. You can duplicate a monitor by double clicking the Monitor ID and changing it.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: 'p404'
                             }, {
                                 name: "Name",
                                 description: "This is the human-readable display name for the monitor.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: 'p404'
                             }, {
                                 name: "Retry Connection",
                                 description: "The number of times to retry for network connection between the server and camera before setting the monitor to Disabled. No decimals. Set to 0 to retry forever.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: ''
                             }, {
                                 name: "Notes",
                                 description: "Comments you want to leave for this camera.",
-                                selectOption: []
+                                selectOption: [],
+                                curVal: ''
                             }, {
                                 name: "Storage Location",
                                 description: "",
-                                selectOption: ['option1', 'option2', 'option3']
+                                selectOption: ['main', 'second'],
+                                curVal: 'main'
                             }
                         ]
                     }
