23cf8dede58095b28b3c8860bb9e94d2636d35b5
Bui Minh Quang <bmq928@gmail.com>
2018-03-23 16:08:22 +0700

    add constants.VIEWS and sync views with it

--- app_client/app.config.js
+++ app_client/app.config.js
@@ -7,20 +7,24 @@
     config.$inject = ['$stateProvider', '$urlRouterProvider']
     function config($stateProvider, $urlRouterProvider) {
 
+        var views = window.constants.VIEWS;
+
+        console.log(views.live)
+
         $stateProvider
-            .state('live', {
+            .state(views.live, {
                 url: '/live',
                 templateUrl: './live/live.view.html',
                 controller: 'liveCtrl',
                 controllerAs: 'vm'
             })
-            .state('playback', {
+            .state(views.playback, {
                 url: '/playback',
                 templateUrl: './playback/playback.view.html',
                 controller: 'playbackCtrl',
                 controllerAs: 'vm'
             })
-            .state('setting', {
+            .state(views.setting, {
                 url: '/setting',
                 templateUrl: './setting/setting.view.html',
                 controller: 'settingCtrl',
---
+++ app_client/app.constant.js
@@ -0,0 +1,7 @@
+window.constants = {
+    VIEWS: {
+        live: 'live',
+        playback: 'playback',
+        setting: 'setting'
+    }
+}
