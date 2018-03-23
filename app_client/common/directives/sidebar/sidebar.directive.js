63c8c20845466e5e71a941945c54995864be07d5
Bui Minh Quang <bmq928@gmail.com>
2018-03-23 15:28:16 +0700

    add click effect on sidenav

--- app_client/common/directives/sidebar/sidebar.directive.js
+++ app_client/common/directives/sidebar/sidebar.directive.js
@@ -4,7 +4,19 @@
         .directive('sidebar', function(){
             return {
                 restrict: 'E',
-                templateUrl: '/common/directives/sidebar/sidebar.template.html'
+                templateUrl: '/common/directives/sidebar/sidebar.template.html',
+                link: function(scope, el, attr){
+
+                    //make animation for nav selection
+                    var $li = $('ul.nav > li');
+
+                    $li.on('click', function(){
+                        var $this = $(this);
+                        $li.removeClass('active');
+                        $this.addClass('active');
+                    })
+
+                }
             }
         })
 })()
