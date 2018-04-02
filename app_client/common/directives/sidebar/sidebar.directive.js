27f1475da5da681277f076461fdbaea62b19a20e
Bui Minh Quang <bmq928@gmail.com>
2018-04-02 16:46:15 +0700

    fix sidebar selection at first timenot chose the right tab

--- app_client/common/directives/sidebar/sidebar.directive.js
+++ app_client/common/directives/sidebar/sidebar.directive.js
@@ -1,23 +1,45 @@
-(function(){
+(function () {
     angular
         .module('app')
-        .directive('sidebar', function(){
-            return {
-                restrict: 'E',
-                templateUrl: '/common/directives/sidebar/sidebar.template.html',
-                controller: 'sidebarCtrl as vm',
-                link: function(scope, el, attr){
+        .directive('sidebar', sidebar)
 
-                    //make animation for nav selection
-                    var $li = $('ul.nav > li');
+    sidebar.$inject = ['viewManage']
+    function sidebar(viewManage) {
+        return {
+            restrict: 'E',
+            templateUrl: '/common/directives/sidebar/sidebar.template.html',
+            controller: 'sidebarCtrl as vm',
+            link: function (scope, el, attr) {
 
-                    $li.on('click', function(){
-                        var $this = $(this);
-                        $li.removeClass('active');
+                //decide what is chosen for the first time
+                //first remove all selection
+                //choose only one that is current view
+                var $li = $('ul.nav > li');
+                var curView = viewManage.getCurView();
+                console.log(curView);
+
+                $li.each(function () {
+                    var $this = $(this)
+                    $this.removeClass('active'); //remove all selection
+
+                    if ($this.children().first().attr('ui-sref') === curView.toLowerCase())
                         $this.addClass('active');
-                    })
+                })
 
-                }
+
+
+
+
+                //make animation for nav selection
+
+
+                $li.on('click', function () {
+                    var $this = $(this);
+                    $li.removeClass('active');
+                    $this.addClass('active');
+                })
+
             }
-        })
+        }
+    }
 })()
