fc93e8b6677417403de5c94d46cfe350954a73d6
Bui Minh Quang <bmq928@gmail.com>
2018-04-16 23:03:32 +0700

    add tab-click-effect sidebar

--- app_client/admin/components/sidebar/sidebar.component.js
+++ app_client/admin/components/sidebar/sidebar.component.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     angular
         .module('appAdmin')
         .component('sidebar', {
@@ -10,15 +10,30 @@
             controllerAs: 'vm'
         })
 
-    
-    function sidebarCtrl(){
-        var vm = this;
-        vm.$onInit = function(){
-            vm.tabs = JSON.parse(vm.options);
 
-            console.log(vm.tabs);
+    function sidebarCtrl() {
+        var vm = this;
+
+
+        vm.$onInit = function () {
+            preprocess();
+            init();
+
         }
 
-        
+        //handle tab click
+        //make active-tab click (ui)
+        vm.tabClick = function (tab) {
+            vm.curTab = tab.title;
+        }
+
+        function preprocess() {
+            vm.curTab = '';
+        }
+
+        function init() {
+            vm.tabs = JSON.parse(vm.options);
+            vm.curTab = vm.tabs[0].title;
+        }
     }
 })()
