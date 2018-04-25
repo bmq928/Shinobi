293da5b92ea00702416268a1eac67fafef640637
bmq928 <bmq928@gmail.com>
2018-04-25 23:19:33 +0700

    gotoAdmin if admin, prevent normalUser from admin page

--- app_client/user/services/authentication.service.js
+++ app_client/user/services/authentication.service.js
@@ -18,31 +18,55 @@
                         mail: mail,
                         password: password
                     })
-                    .then(function(res){
+                    .then(function (res) {
                         var token = res.data.token;
                         success(token)
                         $rootScope.$emit(LOGIN_SUCCESS);
+                        gotoAdmin();
+                        
                     })
-                    .catch(function(err){
+                    .catch(function (err) {
                         fail(err.data.message)
                     })
+
+
+                //if the user is root user
+                // goto admin page
+                function gotoAdmin() {
+
+                    if (isRoot()) {
+                        var hash = location.hash;
+                        var url = '/admin/' + hash;
+
+                        location.replace(url);
+                    }
+
+                    function isRoot() {
+                        if (localStorage.getItem('jwt-token')) {
+                            var info = localStorage.getItem('jwt-token').split('.')[1];
+                            return JSON.parse(atob(info)).isRoot;
+                        }
+
+                        return false;
+                    }
+                }
             },
-            isLogin: function(){
-                if(localStorage.getItem('jwt-token')) return true;
+            isLogin: function () {
+                if (localStorage.getItem('jwt-token')) return true;
 
                 return false;
             },
-            logout: function(){
+            logout: function () {
                 localStorage.removeItem('jwt-token');
                 $rootScope.$emit(LOGOUT_SUCCESS);
             },
-            onLoginSuccess: function(callback){
-                $rootScope.$on(LOGIN_SUCCESS, function(e, data){
+            onLoginSuccess: function (callback) {
+                $rootScope.$on(LOGIN_SUCCESS, function (e, data) {
                     callback(data);
                 })
             },
-            onLogoutSuccess: function(callback){
-                $rootScope.$on(LOGOUT_SUCCESS, function(e, data){
+            onLogoutSuccess: function (callback) {
+                $rootScope.$on(LOGOUT_SUCCESS, function (e, data) {
                     callback(data);
                 })
             },
@@ -54,7 +78,7 @@
             //     return data.mail;
             // }
 
-            getUserData: function(){
+            getUserData: function () {
                 var token = localStorage.getItem('jwt-token');
                 var data = JSON.parse(atob(token.split('.')[1]));
                 console.log(data);
